53f56309f5e6e8e9b17d99e37ab5425b36140c49
Aman Raj Singh <68102965+aman220@users.noreply.github.com>
2024-02-10 19:54:01 +0530

    Add files via upload

---
+++ navigation/index.tsx
@@ -0,0 +1,59 @@
+/**
+ * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import Colors from "../constants/Colors";
+import { RootStackParamList } from "../types";
+import SearchScreen from "../screens/SearchScreen";
+import HomeScreen from "../screens/HomeScreen";
+import DetailsScreen from "../screens/DetailsScreen";
+import Products from "../screens/Products";
+import CouponScreen from "../screens/CoupanScreen";
+import Categories from "../screens/Categories";
+import AllProducts from "../screens/AllProducts";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: Colors.background,
+  },
+};
+
+export default function Navigation() {
+  return (
+    <NavigationContainer theme={theme}>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+/**
+ * A root stack navigator is often used for displaying modals on top of all other content.
+ * https://reactnavigation.org/docs/modal
+ */
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+     
+      <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      <Stack.Screen name='SearchScreen' component={SearchScreen} />
+      <Stack.Screen name='DetailsScreen' component={DetailsScreen} />
+      <Stack.Screen name='Products' component={Products} />
+      <Stack.Screen name='CoupanScreen' component={CouponScreen} />
+      <Stack.Screen name="Categories" component={Categories}/>
+      <Stack.Screen name="AllProducts" component={AllProducts}/>
+    </Stack.Navigator>
+  );
+}
